032f09f4c02a79efc3fa7b10088058793c7a73cb
Sumanth Chinthagunta <xmlking@gmail.com>
2023-07-29 12:26:44 -0700

    fix: deps updated [skip ci]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,13 +15,15 @@ pluginManagement {
             mavenCentral()
         }
         dependencies {
-            classpath("org.eclipse.jgit:org.eclipse.jgit:5.13.1.202206130422-r")
+//            classpath("org.eclipse.jgit:org.eclipse.jgit:5.13.1.202206130422-r")
+            classpath("org.eclipse.jgit:org.eclipse.jgit:6.5.0.202303070854-r")
             // For mssql, flyway gradle plugin need driver in `settings.gradle.kts` buildscript
-            classpath("org.flywaydb:flyway-sqlserver:9.14.1")
+            classpath("org.flywaydb:flyway-sqlserver:9.17.0")
         }
         configurations.classpath {
             resolutionStrategy {
-                force("org.eclipse.jgit:org.eclipse.jgit:5.13.1.202206130422-r")
+//                force("org.eclipse.jgit:org.eclipse.jgit:5.13.1.202206130422-r")
+                force("org.eclipse.jgit:org.eclipse.jgit:6.5.0.202303070854-r")
             }
         }
     }
